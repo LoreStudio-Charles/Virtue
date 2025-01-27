e036e21e8bd5214dd1eafd0044ccd1e82c769f4f
Greg McGlathery <gregory.b.mcglathery@gmail.com>
2025-01-26 18:32:03 -0600

    Create MenuItem class
    Created menuitem class for testing (it was put in mistakenly last push)
    Deleted the class (it served it's purpose)
    So basically a few tasks kind of got rolled into one.
    When play is pressed, a start game and quit game button populate, when clicked, a LOG is printed to show button clicked.

--- Source/Virtue/VirtueGameMode.cpp
+++ Source/Virtue/VirtueGameMode.cpp
@@ -2,7 +2,6 @@
 #include "Engine/World.h"
 #include "Blueprint/UserWidget.h"
 #include "Widgets/SWeakWidget.h"
-#include "SMainMenu.h"
 #include "SMainMenuWidget.h"
 
 AVirtueGameMode::AVirtueGameMode()
@@ -14,14 +13,31 @@ void AVirtueGameMode::StartPlay()
 {
      Super::StartPlay();
 
-     // Log to confirm the game mode has started
+     // Is Slate running?
+     if (FSlateApplication::IsInitialized())
+     {
+          UE_LOG(LogTemp, Warning, TEXT("Slate Application is Initialized"));
+     }
+     else
+     {
+          UE_LOG(LogTemp, Error, TEXT("Slate Application is NOT Initialized"));
+     }
+
+     // Log to confirm Virtue Game Mode has started
      UE_LOG(LogTemp, Warning, TEXT("Virtue Game Mode Started"));
 
      if (GEngine && GEngine->GameViewport)
      {
-          TSharedPtr<SMainMenuWidget> MainMenuWidget = SNew(SMainMenuWidget);
-          GEngine->GameViewport->AddViewportWidgetContent(
-               SNew(SWeakWidget).PossiblyNullContent(MainMenuWidget.ToSharedRef())
-          );
+          TSharedPtr<SMainMenuWidget> MainMenuWidget = SNew(SMainMenuWidget)
+               .Visibility(EVisibility::Visible);
+          GEngine->GameViewport->AddViewportWidgetContent(MainMenuWidget.ToSharedRef());
+
+          // TROUBLESHOOTING: Looking for the viewport to be added
+          UE_LOG(LogTemp, Warning, TEXT("Main Menu Widget Added to Viewport"));
+     }
+     else
+     {
+          // // TROUBLESHOOTING: Just a check
+          UE_LOG(LogTemp, Error, TEXT("GEngine or GameViewport is null!"));
      }
 }
